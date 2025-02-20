479470719719acc26229b9d836252248dad3782c
Luigi Santagada <luigi.santagada@live.com>
2025-02-20 09:41:33 +0100

    .

--- AI_battery_analyser/AI_battery_analyser.ino
+++ AI_battery_analyser/AI_battery_analyser.ino
@@ -1,4 +1,4 @@
-/*
+﻿/*
  Name:		AI_battery_analyser.ino
  Created:	2/18/2025 12:52:20 PM
  Author:	luigi.santagada
@@ -21,19 +21,21 @@ float W1[numberOf_X][numberOf_H] = { 0.00 };
 float W2[numberOf_H][numberOf_Y] = { 0.00 };
 float x[numberOf_X] = { 0.00 };
 float h[numberOf_H] = { 0.00 };
-float y[numberOf_Y] = { 0.00 };
+float y[numberOf_Y] = { 0.00f };
 float processed_data[6] = { 0.00 };
 void read_weights_from_eeprom();
 // the setup function runs once when you press reset or power the board
 void setup() {
 	Serial.begin(9600);
 	read_weights_from_eeprom();
-	processed_data[0] = 1.95f;
-	processed_data[1] = 1.29f;
-	processed_data[2] = 2.13f;
-	processed_data[3] = 1.89f;
-	processed_data[4] = 2.12f;
-	processed_data[5] = 2.14f;
+	/*x[0] = 23.00f;
+	x[1] = 246.00f;*/
+	processed_data[0] = 1.75f;
+	processed_data[1] = 1.34f;
+	processed_data[2] = 1.99f;
+	processed_data[3] = 1.90f;
+	processed_data[4] = 1.85f;
+	processed_data[5] = 1.93f;
 }
 // the loop function runs over and over again until power down or reset
 void loop() {
@@ -46,13 +48,19 @@ void loop() {
 	x[0] = log(x[0] + 1.0f) / 10.0f;
 	x[1] = log(x[1] + 1.0f) / 10.0f;
 	forward();
-	//print_result();
-	normalizeArray(processed_data, y_normalized_real_data, 6);
-	normalizeArray(y, y_normalized_model_data, 6);
-	//print_normalizer_processed_data();
-	//print_normalizer_model_data();
+	print_model_data();
+	normalizeArray(processed_data, y_normalized_real_data, numberOf_Y);
+	normalizeArray(y, y_normalized_model_data, numberOf_Y);
+	float mse = meanSquaredError(y_normalized_real_data, y_normalized_model_data, numberOf_Y);
+	float overall_mean = overallMean(y_normalized_real_data, y_normalized_model_data,numberOf_Y);
+	uint8_t percentage = calculateErrorPercentage(mse, overall_mean);
+	Serial.println(percentage);
+	Serial.println(mse, 10);
+	print_normalizer_processed_data();
+	print_normalizer_model_data();
+	delay(2000);
 }
-void print_result(){
+void print_model_data(){
 	Serial.println();
 	Serial.print(F("x[0] = ")); Serial.print(exp(x[0] * 10.00f)); Serial.print(F("    x[1] = ")); Serial.println(exp(x[1] * 10.00f));
 	Serial.print(F("\n\ny[0] = ")); Serial.println(y[0] * 10.00f);
@@ -63,7 +71,6 @@ void print_result(){
 	Serial.print(F("y[5] = ")); Serial.println(y[5] * 10.00f);
 	delay(2000);
 }
-
 void print_normalizer_processed_data() {
 	Serial.println();
 	Serial.print(F("y_normalized[0] = ")); Serial.println(y_normalized_real_data[0]);
@@ -151,6 +158,31 @@ void normalizeArray(float* arr, float* normArr, int size) {
 			normArr[i] = 0; // Evita divisione per zero nel caso di valori uguali
 		}
 	}
+}
+float meanSquaredError(const float* arr1, const float* arr2, int size) {
+	float sum = 0.0f;
+	for (int i = 0; i < size; ++i) {
+		float diff = arr1[i] - arr2[i];
+		sum += diff * diff;  // quadrato della differenza
+	}
+	// MSE = (1 / N) * Σ (diff^2)
+	return sum / size;
+}
+float overallMean(const float* arr1, const float* arr2, int size) {
+	float sum = 0.0f;
+	// Sommiamo tutti gli elementi di entrambi gli array
+	for (int i = 0; i < size; ++i) {
+		sum += arr1[i] + arr2[i];
+	}
+	// La media complessiva è la somma divisa per il numero totale di elementi (2*size)
+	return sum / (2 * size);
+}
+uint8_t calculateErrorPercentage(float mse, float overallMean) {
+	// Calcola il Root Mean Squared Error (RMSE)
+	float rms = sqrt(mse);
+	// Calcola la percentuale: (RMSE / media complessiva) * 100
+	uint8_t errorPercentage = (rms / overallMean) * 100.0f;
+	return errorPercentage;
 }
 int freeMemory() {
 	int free_memory;

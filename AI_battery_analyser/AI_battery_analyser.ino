eae00c92027e954570b9c9f99bd92d51bb48737e
Luigi Santagada <luigi.santagada@live.com>
2025-02-20 01:19:00 +0100

    .

--- AI_battery_analyser/AI_battery_analyser.ino
+++ AI_battery_analyser/AI_battery_analyser.ino
@@ -3,11 +3,18 @@
  Created:	2/18/2025 12:52:20 PM
  Author:	luigi.santagada
 */
-
+#include <Arduino.h>
+#ifndef RAMSTART
+extern int __data_start;
+#endif
+extern int __bss_end;
+extern void* __brkval;
 #include <EEPROM.h>
 const uint8_t numberOf_X = 2;
 const uint8_t numberOf_H = 25;
 const uint8_t numberOf_Y = 6;
+float y_normalized_real_data[numberOf_Y] = { 0.00 };
+float y_normalized_model_data[numberOf_Y] = { 0.00 };
 float output_bias[numberOf_Y] = { 0.00 };
 float hidden_bias[numberOf_H] = { 0.00 };
 float W1[numberOf_X][numberOf_H] = { 0.00 };
@@ -15,30 +22,67 @@ float W2[numberOf_H][numberOf_Y] = { 0.00 };
 float x[numberOf_X] = { 0.00 };
 float h[numberOf_H] = { 0.00 };
 float y[numberOf_Y] = { 0.00 };
+float processed_data[6] = { 0.00 };
+void read_weights_from_eeprom();
 // the setup function runs once when you press reset or power the board
 void setup() {
 	Serial.begin(9600);
-	pinMode(8, OUTPUT);
-	digitalWrite(8, LOW);
 	read_weights_from_eeprom();
-	delay(500);
-	x[0] = 30.00f;
-	x[1] = 500.00f;
-	x[0] = log(x[0] + 1.0f) / 10.0f;
-	x[1] = log(x[1] + 1.0f) / 10.0f;
-	forward();
-	Serial.println();
-	Serial.print("x[0] = ");Serial.print(exp(x[0] * 10.00f));Serial.print("    x[1] = ");Serial.println(exp(x[1] * 10.00f));
-	Serial.print("\n\ny[0] = ");Serial.println(y[0] * 10.00f);
-	Serial.print("y[1] = ");Serial.println(y[1] * 10.00f);
-	Serial.print("y[2] = ");Serial.println(y[2] * 10.00f);
-	Serial.print("y[3] = ");Serial.println(y[3] * 10.00f);
-	Serial.print("y[4] = ");Serial.println(y[4] * 10.00f);
-	Serial.print("y[5] = ");Serial.println(y[5] * 10.00f);
-	//digitalWrite(8, HIGH);
+	processed_data[0] = 1.95f;
+	processed_data[1] = 1.29f;
+	processed_data[2] = 2.13f;
+	processed_data[3] = 1.89f;
+	processed_data[4] = 2.12f;
+	processed_data[5] = 2.14f;
 }
 // the loop function runs over and over again until power down or reset
 void loop() {
+	int ram_libera = freeMemory();
+	Serial.print(F("RAM libera: "));
+	Serial.print(ram_libera);
+	Serial.println(F(" byte"));
+	x[0] = 23.00f;
+	x[1] = 246.00f;
+	x[0] = log(x[0] + 1.0f) / 10.0f;
+	x[1] = log(x[1] + 1.0f) / 10.0f;
+	forward();
+	//print_result();
+	normalizeArray(processed_data, y_normalized_real_data, 6);
+	normalizeArray(y, y_normalized_model_data, 6);
+	//print_normalizer_processed_data();
+	//print_normalizer_model_data();
+}
+void print_result(){
+	Serial.println();
+	Serial.print(F("x[0] = ")); Serial.print(exp(x[0] * 10.00f)); Serial.print(F("    x[1] = ")); Serial.println(exp(x[1] * 10.00f));
+	Serial.print(F("\n\ny[0] = ")); Serial.println(y[0] * 10.00f);
+	Serial.print(F("y[1] = ")); Serial.println(y[1] * 10.00f);
+	Serial.print(F("y[2] = ")); Serial.println(y[2] * 10.00f);
+	Serial.print(F("y[3] = ")); Serial.println(y[3] * 10.00f);
+	Serial.print(F("y[4] = ")); Serial.println(y[4] * 10.00f);
+	Serial.print(F("y[5] = ")); Serial.println(y[5] * 10.00f);
+	delay(2000);
+}
+
+void print_normalizer_processed_data() {
+	Serial.println();
+	Serial.print(F("y_normalized[0] = ")); Serial.println(y_normalized_real_data[0]);
+	Serial.print(F("y_normalized[1] = "));  Serial.println(y_normalized_real_data[1]);
+	Serial.print(F("y_normalized[2] = "));  Serial.println(y_normalized_real_data[2]);
+	Serial.print(F("y_normalized[3] = "));  Serial.println(y_normalized_real_data[3]);
+	Serial.print(F("y_normalized[4] = "));  Serial.println(y_normalized_real_data[4]);
+	Serial.print(F("y_normalized[5] = "));  Serial.println(y_normalized_real_data[5]);
+	delay(2000);
+}
+void print_normalizer_model_data() {
+	Serial.println();
+	Serial.print(F("y_normalized_model_data[0] = ")); Serial.println(y_normalized_model_data[0]);
+	Serial.print(F("y_normalized_model_data[1] = "));  Serial.println(y_normalized_model_data[1]);
+	Serial.print(F("y_normalized_model_data[2] = "));  Serial.println(y_normalized_model_data[2]);
+	Serial.print(F("y_normalized_model_data[3] = "));  Serial.println(y_normalized_model_data[3]);
+	Serial.print(F("y_normalized_model_data[4] = "));  Serial.println(y_normalized_model_data[4]);
+	Serial.print(F("y_normalized_model_data[5] = "));  Serial.println(y_normalized_model_data[5]);
+	delay(2000);
 }
 float relu(float x) {
 	return (x > 0) ? x : 0;
@@ -89,4 +133,32 @@ void read_weights_from_eeprom() {
 		EEPROM.get(addr, output_bias[j]);
 		addr += sizeof(float);
 	}
+}
+void normalizeArray(float* arr, float* normArr, int size) {
+	float minVal = arr[0];
+	float maxVal = arr[0];
+	// Trova il minimo e il massimo
+	for (int i = 1; i < size; i++) {
+		if (arr[i] < minVal) minVal = arr[i];
+		if (arr[i] > maxVal) maxVal = arr[i];
+	}
+	// Normalizza i valori
+	for (int i = 0; i < size; i++) {
+		if (maxVal != minVal) {
+			normArr[i] = (arr[i] - minVal) / (maxVal - minVal);
+		}
+		else {
+			normArr[i] = 0; // Evita divisione per zero nel caso di valori uguali
+		}
+	}
+}
+int freeMemory() {
+	int free_memory;
+	if ((int)__brkval == 0) {
+		free_memory = ((int)&free_memory) - ((int)&__bss_end);
+	}
+	else {
+		free_memory = ((int)&free_memory) - ((int)__brkval);
+	}
+	return free_memory;
 }
